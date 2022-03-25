a40364d798fb8b55c5416e86ddb95c3df97243c0
Colin van Loo <colin.vanloo@edu.tbz.ch>
2022-03-25 08:24:52 +0100

    feat: server receive requests

--- Server/main.go
+++ Server/main.go
@@ -1,25 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
-	"framagit.org/edu-cvl/M242-LB/API/api"
+	"github.com/grigorypavlov/M242_LB2/Server/api"
+	"github.com/tidwall/buntdb"
 )
 
 func main() {
-	srv := api.Default()
+	db, err := buntdb.Open(":memory:")
+	//db.CreateIndex("sensor_name", "*", buntdb.IndexJSON("sensor.name"))
+	if err != nil {
+		log.Fatalf("Failed opening database: %s", err)
+	}
+	srv := api.New(db)
 
 	mux := http.NewServeMux()
 	mux.Handle(
 		"/api/v1/hello_world",
 		loggingMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			err := srv.PostData(w, r)
+			err := srv.PostUpdateReport(w, r)
 			if err != nil {
 				log.Printf("Error handling request: %v, %v\n", r, err)
 			}
 		})))
 
+	mux.Handle(
+		"/api/v1/data",
+		loggingMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			err := srv.GetDataUpdate(w, r)
+			if err != nil {
+				log.Printf("Error handling request: %v, %v\n", r, err)
+			}
+		})))
+
+	fmt.Println("Listening on :8080 ...")
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
 
