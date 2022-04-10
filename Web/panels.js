2df38e386b4ae3ec594d5ceec4aee362a5be6a5b
Colin van Loo <colin.vanloo@edu.tbz.ch>
2022-04-10 17:35:04 +0200

    feat: gui for web interface

---
+++ Web/panels.js
@@ -0,0 +1,63 @@
+let offsetX, offsetY;
+let selectedWindow;
+
+const getCursorPosition = (e) => {
+    cursorX = (window.Event) ? e.pageX :
+        event.clientX + (document.documentElement.scrollLeft ?
+            document.documentElement.scrollLeft :
+            document.body.scrollLeft);
+
+    cursorY = (window.Event) ? e.pageY :
+        event.clientY + (document.documentElement.scrollTop ?
+            document.documentElement.scrollTop :
+            document.body.scrollTop);
+
+    return {
+        cursorX,
+        cursorY
+    };
+};
+
+const windowToCursorPosition = (w) => {
+    w.style.position = "absolute";
+    w.style.top = cursorY - offsetY + "px";
+    w.style.left = cursorX - offsetX + "px";
+};
+
+const grabWindow = (e, w) => {
+    // Prevent text selection.
+    e.preventDefault();
+
+    // Calculate mouse offset inside window.
+    const target = w;
+    const rect = target.getBoundingClientRect();
+    offsetX = e.clientX - rect.left;
+    offsetY = e.clientY - rect.top;
+
+    selectedWindow = w;
+};
+
+const onWindowMoving = (e) => {
+    const cursor = getCursorPosition(e);
+
+    if (selectedWindow != null) {
+        windowToCursorPosition(selectedWindow);
+    }
+};
+
+const releaseWindow = (e) => {
+    selectedWindow = null;
+};
+
+// Register event handlers.
+const registerWindows = (windows) => {
+    for (const w of windows) {
+        w.onmousedown = (e) => {
+            grabWindow(e, w);
+        };
+    }
+
+    document.onmousemove = onWindowMoving;
+
+    document.onmouseup = releaseWindow;
+};
